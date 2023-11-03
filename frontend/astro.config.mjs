2edb929351a7103af9cbdbceaf24f74811df4d83
RohanJnr <rohanjnr44@gmail.com>
2023-11-03 14:10:44 +0530

    add vercel adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), solidJs()]
 });
